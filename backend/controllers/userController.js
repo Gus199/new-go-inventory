9bd32b57c0fbd528d97a9df5ebce7ef7daad47e1
Your Name <apple@MacBook-Pro.fios-router.home>
2022-06-17 15:44:43 -0400

    Add token and protect routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-
+const jwt = require('jsonwebtoken')
 const User = require("../models/userModels");
 // @desc Register new USer
 //@route/api/users
@@ -35,6 +35,7 @@ const regierUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -46,10 +47,44 @@ const regierUser = asyncHandler(async (req, res) => {
 //@route/api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("Login Route");
+    const {email, password} = req.body
+     // Check user and password
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id)
+    })
+    }else {
+        res.status(401) 
+        throw new Error('Invalid Credentials')
+    }
+
 });
+// @desc Gst current User new USer
+//@route/api/me
+// @access Public
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    }
+ res.status(200).json(user)
+})
+
+// Generate token
+
+const generateToken = (id) => {
+ return jwt.sign({ id }, process.env.JWT_SECRET,  {
+     expiresIn: '30d'
+ })
+}
 
 module.exports = {
   regierUser,
   loginUser,
+  getMe
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { regierUser, loginUser } = require('../controllers/userController')
+const {
+  regierUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
+
+const {protect} = require('../middleware/authMiddleware')
 
 router.post("/", regierUser);
 
 router.post("/login", loginUser);
+
+router.get("/me",protect, getMe);
 
 module.exports = router;
