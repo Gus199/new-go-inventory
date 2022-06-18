c6018919aa328c6e50d34276ede656ba0ff0a98a
Your Name <apple@MacBook-Pro.fios-router.home>
2022-06-18 12:41:33 -0400

    connect frontend to backend using redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 
@@ -19,6 +21,7 @@ function App() {
       </Routes>
     </div>
   </Router>
+  <ToastContainer />
   
   </>
     
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaQuestionCircle,  FaTicketAlt} from 'react-icons/fa'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+    <section className='heading'>
+        <h1>What do you need help with</h1>
+        <p>Please choose from an option below</p>
+    </section>
+  
+    <Link to='/new-item' className='btn btn-reverse btn-block'>
+   <FaQuestionCircle /> Create new Item
+    </Link>
+    <Link to='/items' className='btn  btn-block'>
+   <FaTicketAlt /> View my Items
+    </Link>
+  
+    </>
+
   )
 }
 
